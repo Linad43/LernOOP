980fe92f3fbfbb192c4d4cda78875e2b72a3b032
Linad <merzlikindaniil97@gmail.com>
2026-06-01 11:43:59 +0300

    HM_16_1 done

---
+++ src/model/LawnGrass.py
@@ -0,0 +1,22 @@
+from src.model.Product import Product
+
+
+class LawnGrass(Product):
+    country: str
+    germination_period: int
+    color: str
+
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            price: float,
+            quantity: int,
+            country: str,
+            germination_period: int,
+            color: str
+    ) -> None:
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
